cdf518b1e3c0b5dfcdacab1513e16f8b3b16b294
DW-TheWeasel <36249483+DW-TheWeasel@users.noreply.github.com>
2024-05-27 13:43:32 -0700

    Highscore
    Added highscore list and user management. Works with mysql if available otherwise sqlite3 for local db

--- resources/lib/quizlib/logger.py
+++ resources/lib/quizlib/logger.py
@@ -1,4 +1,9 @@
 import xbmc
+import xbmcaddon
 
 def log(message, level=xbmc.LOGDEBUG):
     xbmc.log("script.moviequiz >> " + message, level)
+    
+def notification(message, duration=2000):
+    icon = xbmcaddon.Addon().getAddonInfo('icon')
+    xbmc.executebuiltin('Notification(MovieQuiz, %s, %d, %s)'%(message, duration, icon))
